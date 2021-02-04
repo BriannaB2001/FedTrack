7fe9053a34fca0f12b3c4987a58d17630814dbd0
Brianna <nanitooze@gmail.com>
2021-02-04 12:11:18 -0700

    Api Started, Api URL Started

---
+++ FedTrack/AllBills.swift
@@ -0,0 +1,8 @@
+//
+//  AllBills.swift
+//  FedTrack
+//
+//  Created by Brianna Babcock on 2/4/21.
+//
+
+import Foundation
---
+++ FedTrack/AllBillsURLController.swift
@@ -0,0 +1,8 @@
+//
+//  AllBillsURLController.swift
+//  FedTrack
+//
+//  Created by Brianna Babcock on 2/4/21.
+//
+
+import Foundation
