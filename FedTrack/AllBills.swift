4979dd004c0a80702efc9acf598f22fba93b0bd2
Brianna <nanitooze@gmail.com>
2021-02-04 12:11:38 -0700

    Api Started, Api URL Started

--- FedTrack/AllBills.swift
+++ FedTrack/AllBills.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct AllBills: Codable {
+    var results: [Bills]
+}
+
+struct Bills: Codable {
+    var bills: [Bill]
+}
+
+struct Bill: Codable {
+    
+    var number: String
+    var shortTitle: String
+    var summary: String
+    var primarySubject: String
+    var lastVote: String
+    var sponsorName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case number = "number"
+        case shortTitle = "short_title"
+        case summary = "summary"
+        case primarySubject = "primary_subject"
+        case lastVote = "last_vote"
+        case sponsorName = "sponsor_name"
+    }
+}
--- FedTrack/AllBillsURLController.swift
+++ FedTrack/AllBillsURLController.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class BillsURLController {
+    
+    static func fetchBillsItems(completion: @escaping ([Bills]?) -> Void) {
+        guard let baseURL = URL(string: "https://api.propublica.org/congress/v1/bills/search.json?query=megahertz") else { fatalError() }
+        let key = "VBuip8RlZmugied6nsDVbd8j1b4fxJ2Y3kiFrgNd"
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "GET"
+        request.addValue(key, forHTTPHeaderField: "X-API-Key")
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(data)
+        }.resume()
+        
+        let task = URLSession.shared.dataTask(with: baseURL) { (data, resposne, error) in
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                if let data = data {
+                    let billInfo = try jsonDecoder.decode(AllBills.self, from: data)
+                    completion(billInfo.results)
+                } else {
+                    completion(nil)
+                }
+            } catch {
+                print (error)
+            }
+            
+        }
+        task.resume()
+    }
+}
