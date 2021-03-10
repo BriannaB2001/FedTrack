0562b3e44bb23c2897a6cfeeb2de2f9138918de1
Brianna <nanitooze@gmail.com>
2021-03-10 12:05:50 -0700

    Tab Bar working only for Bills storybaord, Bills view only showing images

--- FedTrack/Overview/OverviewTableViewController.swift
+++ FedTrack/Overview/OverviewTableViewController.swift
@@ -7,59 +7,51 @@
 import UIKit
 import SwiftUI
 
-class OverviewTableViewController: UITableViewController, UIPopoverPresentationControllerDelegate, SubjectChosen {
+class OverviewTableViewController: UITableViewController, UIPopoverPresentationControllerDelegate  {
     
-    var allBills = [Bills]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        AllBillsURLController.fetchBillsItems { (bills) in
-            DispatchQueue.main.async {
-                self.allBills = bills ?? []
-                self.tableView.reloadData()
-            }
-        }
-        
-    }
-    
-    // Mark: - Subject Chosen Delegate
-    
-    func subjectBills(bill: [Bills]) {
-        allBills = bill
-        tableView.reloadData()
-    }
-    
-    // MARK: - Table view data source
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if allBills.count > 0 {
-            return allBills.first!.bills.count
-        } else {
-            return 0
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "overviewCell", for: indexPath) as! BillsTableViewCell
-        
-        let bill = allBills.first!.bills[indexPath.row]
-        cell.updateCell(bill: bill)
-        cell.updateImage(bill: bill)
-        
-        let congressView = UIHostingController(rootView: ContentView(committee: true, house: true, senate: true, enacted: false))
-        cell.contentView.translatesAutoresizingMaskIntoConstraints = false
-        congressView.view.frame = cell.contentView.bounds
-        cell.contentView.addSubview(congressView.view)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt
-                                indexPath: IndexPath) -> CGFloat {
-        let normalCellHeight = CGFloat(90)
-        
-        return normalCellHeight
-    }
+//    var allBills = [Bills]()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        AllBillsURLController.fetchBillsItems { (bills) in
+//            DispatchQueue.main.async {
+//                self.allBills = bills ?? []
+//                self.tableView.reloadData()
+//            }
+//        }
+//    }
+//
+//
+//    // MARK: - Table view data source
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if allBills.count > 0 {
+//            return allBills.first!.bills.count
+//        } else {
+//            return 0
+//        }
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "overviewCell", for: indexPath) as! BillsTableViewCell
+//
+//        let bill = allBills.first!.bills[indexPath.row]
+//        cell.updateCell(bill: bill)
+//
+//        let congressView = UIHostingController(rootView: ContentView(committee: true, house: true, senate: true, enacted: false))
+//        cell.contentView.translatesAutoresizingMaskIntoConstraints = false
+//        congressView.view.frame = cell.contentView.bounds
+//        cell.contentView.addSubview(congressView.view)
+//
+//        return cell
+//    }
+//
+//    override func tableView(_ tableView: UITableView, heightForRowAt
+//                                indexPath: IndexPath) -> CGFloat {
+//        let normalCellHeight = CGFloat(90)
+//
+//        return normalCellHeight
+//    }
     
 }
